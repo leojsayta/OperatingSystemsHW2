6d3d0180f69ebb0726414fc6c5baf33d0a08c612
Joel Atyas <jatyas@gmail.com>
2015-04-10 14:10:46 -0400

    Initial Commit

---
+++ HW2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  HW2
+//
+//  Created by Joel Atyas on 4/10/15.
+//  Copyright (c) 2015 Joel Atyas. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
