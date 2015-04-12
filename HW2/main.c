5e247d58b5c2685555634340d4ebe990555e9ff3
Joel Atyas <jatyas@gmail.com>
2015-04-11 23:16:15 -0400

    More updates. Works correctly with pthread_mutex, but not sem_t

--- HW2/main.c
+++ HW2/main.c
@@ -19,12 +19,20 @@
 #define NUM_READS 200
 #define NUM_WRITES 200
 
-pthread_mutex_t readerCountMutex;      // the mutual exclusion semaphore for the readerCount
+//#define SEMTYPE
+
 int readerCount;             // number of threads reading or wanting to read
+pthread_mutex_t readerCountMutex;      // the mutual exclusion semaphore for the readerCount
 
 int isWriterWaiting;
 pthread_cond_t readerWait;
+
+#ifdef SEMTYPE
+sem_t* bufferSem;          // mutual exclusion semaphore for the "shared buffer"
+#else
 pthread_mutex_t bufferMutex;          // mutual exclusion semaphore for the "shared buffer"
+#endif
+
 int sharedBuffer;           // the "shared buffer" is an int starting at 0.
                             // readers read its value and print out.
                             // writer increments the value by 1.
@@ -33,64 +41,99 @@ void* writerCode(void* a)
 {
     struct timespec ts;
     ts.tv_sec = 0;
-    ts.tv_nsec = 10;
+    ts.tv_nsec = 25000;
+//    int semVal = 0;
     
     int count;
     for (count = 0; count < NUM_WRITES; count++)
     {
-        isWriterWaiting = 1;
-        pthread_mutex_lock(&bufferMutex);
         
+
+#ifdef SEMTYPE
+        sem_wait(bufferSem);
+//        printf("The writer (pre sharedBuffer incr) bufferSem value is %d.\n", sem_getvalue(bufferSem, &semVal));
+#else
+        if (pthread_mutex_trylock(&bufferMutex)) {
+            isWriterWaiting = 1;
+            pthread_mutex_lock(&bufferMutex);
+        }
+#endif
         sharedBuffer = sharedBuffer + 1;
         
         printf("Writer is on iteration %d. The new buffer value is %d. The readerCount is %d\n\n", count + 1, sharedBuffer, readerCount);
-//        printf("New buffer value is %d.\n", sharedBuffer);
         
         isWriterWaiting = 0;
-        pthread_cond_signal(&readerWait);
-        pthread_mutex_unlock(&bufferMutex);
-//        nanosleep(&ts, NULL);
+        pthread_cond_broadcast(&readerWait);
+        
+#ifdef SEMTYPE
+        sem_post(bufferSem);
+//        printf("The writer (post sharedBuffer incr) bufferSem value is %d.\n", sem_getvalue(bufferSem, &semVal));
+#else
+        pthread_mutex_unlock(&bufferMutex);        
+#endif
+        nanosleep(&ts, NULL);
     }
     
     pthread_exit(0);
-    
-    //return 0;
 }
 
 void* readerCode(void* a)
 {
-    int argumentValue = *((int *) a); // dereference to obtain argument value
+    int argumentValue = *((int *) a) + 1; // dereference to obtain argument value
     //printf("Greetings from Reader: %d.\n", argumentValue);
     
+//    struct timespec ts;
+//    ts.tv_sec = 0;
+//    ts.tv_nsec = 25000;
+//    int semVal = 0;
+
     int count;
     for (count = 0; count < NUM_READS; count++)
     {
         pthread_mutex_lock(&readerCountMutex);
         
-        if (isWriterWaiting && readerCount == 5) {
+        while (isWriterWaiting)
             pthread_cond_wait(&readerWait, &readerCountMutex);
+        
+        if (readerCount == 0)
+        {
+#ifdef SEMTYPE
+            sem_wait(bufferSem);
+//            printf("The reader bufferSem value (pre-read) is %d.\n", sem_getvalue(bufferSem, &semVal));
+#else
+            pthread_mutex_lock(&bufferMutex);            
+#endif
         }
         
         readerCount = readerCount + 1;
-        if (readerCount == 1)
-            pthread_mutex_lock(&bufferMutex);
-        printf("The ++readerCount is %d.\n", readerCount);
+        //        printf("The ++readerCount is %d.\n", readerCount);
+        
         pthread_mutex_unlock(&readerCountMutex);
         
-        printf("Reader %d is on iteration %d. The current buffer value is %d\n", argumentValue, count + 1, sharedBuffer);
+        printf("Reader %d is on iteration %d. The current buffer value is %d. The readerCount is %d.\n\n",
+               argumentValue, count + 1, sharedBuffer, readerCount);
         
         pthread_mutex_lock(&readerCountMutex);
+        
         readerCount = readerCount - 1;
+//        printf("The --readerCount is %d.\n\n", readerCount);
+        
         if (readerCount == 0)
-            pthread_mutex_unlock(&bufferMutex);
-        printf("The --readerCount is %d.\n\n", readerCount);
+        {
+#ifdef SEMTYPE
+            sem_post(bufferSem);
+            //            printf("The reader bufferSem value (post-read) is %d.\n", sem_getvalue(bufferSem, &semVal));
+#else
+            pthread_mutex_unlock(&bufferMutex);            
+#endif
+        }
+
         pthread_mutex_unlock(&readerCountMutex);
+//        nanosleep(&ts, NULL);
     }
     
     pthread_exit(0);
-    
-    //return 0;
-}
+ }
 
 int main()
 {
@@ -98,8 +141,14 @@ int main()
     readerCount = 0;
     
     pthread_mutex_init(&readerCountMutex, 0);
-    pthread_mutex_init(&bufferMutex, 0);
     pthread_cond_init(&readerWait, 0);
+
+#ifdef SEMTYPE
+    bufferSem = sem_open("bufferSem", O_CREAT | O_EXCL, S_IRUSR | S_IWUSR, 1);
+//    sem_init(&bufferSem, 0, 1);
+#else
+    pthread_mutex_init(&bufferMutex, 0);    
+#endif
     
     pthread_t writerThread;
     pthread_create(&writerThread, 0, writerCode, 0);
@@ -119,6 +168,15 @@ int main()
         pthread_join(readerThread[count], 0);
     
     pthread_mutex_destroy(&readerCountMutex);
-    pthread_mutex_destroy(&bufferMutex);
     pthread_cond_destroy(&readerWait);
+    
+#ifdef SEMTYPE
+    sem_close(bufferSem);
+#else
+    pthread_mutex_destroy(&bufferMutex);    
+#endif
+    
+    
+    
+    
 }
